177fb0d4d4fef049b450ad25ea8e6af4258d4d8a
Tony Bowden <tony@tmtm.com>
2021-08-07 14:00:47 +0300

    Update to every-politician-scraper 0.1.1

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -5,45 +5,25 @@ require 'every_politician_scraper/scraper_data'
 require 'pry'
 
 class MemberList
-  # details for an individual member
-  class Member < Scraped::HTML
-    PREFIXES = %w[Senator The Hon Dame Lt Col Mr Mrs Ms Dr].freeze
-    SUFFIXES = %w[M.P. J.P. Q.C. D.A. GCMG].freeze
-
-    field :name do
-      SUFFIXES.reduce(unprefixed_name) { |current, suffix| current.sub(/#{suffix}/, '').tidy.sub(/,$/, '') }.sub(/\.$/, '')
+  class Member
+    def name
+      Name.new(
+        full:     noko.css('.c-name').text,
+        prefixes: %w[Senator The Hon Dame Lt Col Mr Mrs Ms Dr],
+        suffixes: %w[M.P. J.P. Q.C. D.A. GCMG]
+      ).short.tidy.sub(/,$/, '').sub(/\.$/, '')
     end
 
-    field :position do
+    def position
       # These two are only in the body text
       return 'Governor-General' if name == 'Sandra Prunella Mason'
       return 'Prime Minister' if name == 'Mia Amor Mottley'
 
       noko.css('.c-position').text.tidy
     end
-
-    private
-
-    def full_name
-      noko.css('.c-name').text.tidy
-    end
-
-    def unprefixed_name
-      PREFIXES.reduce(full_name) { |current, prefix| current.sub(/#{prefix}\.? /, '') }
-    end
   end
 
-  # The page listing all the members
-  class Members < Scraped::HTML
-    field :members do
-      member_container.flat_map do |member|
-        data = fragment(member => Member).to_h
-        [data.delete(:position)].flatten.map { |posn| data.merge(position: posn) }
-      end
-    end
-
-    private
-
+  class Members
     def member_container
       noko.css('.c-body')
     end
